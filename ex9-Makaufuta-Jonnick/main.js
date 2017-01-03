73d4ac969a58ed6593e753827c18fbe51c7d3288
jonick makau <r0591009@student.thomasmore.be>
2017-01-03 22:08:09 +0100

    ik heb mijn data geimporteerd naar de module

---
+++ ex9-Makaufuta-Jonnick/main.js
@@ -0,0 +1,5 @@
+//hier zal ik een mijn data importeren naar de module
+var parser = require('body-parser');//BODY UIT LEZEN, EXTENSIE OP EXPRESS
+var mongoose = require('mongoose');//GELIJKACHTIG AAN MONGOCLIENT --> JONAS
+var express = require('express');//WEBSERVER NODE JS
+
