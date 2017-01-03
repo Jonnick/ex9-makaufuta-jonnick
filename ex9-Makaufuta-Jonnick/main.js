743cc7d6b8adda97f0ee170b54c1601c9204cdd0
jonick makau <r0591009@student.thomasmore.be>
2017-01-03 22:22:46 +0100

    nu ga ik mijn data in een plaats bewaren en dat zal ik ook aanmaken

--- ex9-Makaufuta-Jonnick/main.js
+++ ex9-Makaufuta-Jonnick/main.js
@@ -5,3 +5,17 @@ var express = require('express');//WEBSERVER NODE JS
 
 //connectie naar mongodb
 mongoose.connect('mongodb://localhost:27017/MyAPI');//DATABANK IN ROBOMONGO
+
+//nu ga ik mijn data in een plaats bewaren en dat zal ik ook aanmaken
+
+//movement
+var dalBeweging = require('./storageBewegingen.js');
+var validationBewegingen = require('./validateBewegingen.js');
+
+//locatie
+var dalLocation = require('./storageLocations.js');
+var validationLocations = require('./validateLocations.js');
+
+//availleble
+var dalAanwezig = require('./storageAanwezigheden.js');
+var validationAanwezigheden = require('./validateAanwezigheden.js');
