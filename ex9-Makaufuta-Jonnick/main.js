6e06a5cc33cf8bc98366a106398a1f9931dfc1ed
jonick makau <r0591009@student.thomasmore.be>
2017-01-03 22:10:08 +0100

    ik heb connectie aangemaken met mijn mongodb via mijn robotmongo dat ik heb moeten downloaden en installeren

--- ex9-Makaufuta-Jonnick/main.js
+++ ex9-Makaufuta-Jonnick/main.js
@@ -3,3 +3,5 @@ var parser = require('body-parser');//BODY UIT LEZEN, EXTENSIE OP EXPRESS
 var mongoose = require('mongoose');//GELIJKACHTIG AAN MONGOCLIENT --> JONAS
 var express = require('express');//WEBSERVER NODE JS
 
+//connectie naar mongodb
+mongoose.connect('mongodb://localhost:27017/MyAPI');//DATABANK IN ROBOMONGO
